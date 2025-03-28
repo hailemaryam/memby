7b027b51d86a1ed82cf1cbe1d02cce2c7721cb62
hailemaryam <hailemaryammecca@gmail.com>
2025-03-28 19:48:28 +0300

    child tables creataed

---
+++ mewacho/mewacho/doctype/mewacho_payment/mewacho_payment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, hailemaryammecca@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mewacho Payment", {
+// 	refresh(frm) {
+
+// 	},
+// });
