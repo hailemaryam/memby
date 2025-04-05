5f3cfc7185dc7293bd01e77882c8797b3f9b075b
hailemaryam <hailemaryammecca@gmail.com>
2025-04-05 16:53:51 +0300

    payment term generation refactored

--- mewacho/mewacho/my_custom_functions/payment_term_creator.py
+++ mewacho/mewacho/my_custom_functions/payment_term_creator.py
@@ -1,37 +1,35 @@
 import frappe
-from datetime import timedelta
-from frappe.utils import getdate
+from frappe.utils import getdate, add_days, today
 
 def create_payment_terms_for_members():
-    members = frappe.get_all('Mewacho Member', filters={'registration_date': ['is', 'set']})    
+    members = frappe.db.sql("""
+        SELECT name, registration_date, membership_fee, last_created_end_date
+        FROM `tabMewacho Member`
+        WHERE (last_created_end_date <= %s OR last_created_end_date IS NULL)
+    """, (today(),), as_dict=True)
     for member in members:
         member_doc = frappe.get_doc('Mewacho Member', member.name)
-        existing_terms = frappe.get_all('Mewacho Payment Term', filters={'parent': member.name}, order_by='end_date desc')
-        if not existing_terms:
-            if member_doc.registration_date.date() > getdate():
-                frappe.log_error(f"Skipping Payment Term creation for Member: {member.name} due to future registration date.", "Payment Term Creation")
-                continue  # Skip payment term creation for this member                
-            create_payment_terms(member_doc, member_doc.registration_date)
-        else:
-            last_end_date = existing_terms[0].get('end_date')
-            if last_end_date and (getdate() - last_end_date.date()).days > 30:
-                create_payment_terms(member_doc, last_end_date)
+        create_payment_terms(member_doc, member_doc.last_created_end_date)
 
 def create_payment_terms(member, start_date):
     if not start_date:
-        start_date = member.registration_date    
-    while start_date.date() < getdate():
-        end_date = start_date + timedelta(days=30)
+        start_date = member.registration_date
+    while start_date < getdate():
+        end_date = add_days(start_date, 30)
         payment_term = frappe.get_doc({
             'doctype': 'Mewacho Payment Term',
             'parent': member.name,
             'parenttype': 'Mewacho Member',
             'parentfield': 'payment_terms',
-            'amount': member.memeber_ship_fee,
+            'amount': member.membership_fee,
             'is_paid': False,
+            'penalized': False,
             'start_date': start_date,
             'end_date': end_date
         })
-        payment_term.insert(ignore_permissions=True)
-        frappe.db.commit()
+        member.append('payment_terms', payment_term)
         start_date = end_date
+    member.last_created_end_date = start_date
+    member.save()
+    frappe.db.commit()
+
