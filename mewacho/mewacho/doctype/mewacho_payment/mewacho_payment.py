b0414cef434fe0345f6c8de6d6bf5d3e57f3fc12
hailemaryam <hailemaryammecca@gmail.com>
2025-04-13 07:17:42 +0300

    initial payment effected added

--- mewacho/mewacho/doctype/mewacho_payment/mewacho_payment.py
+++ mewacho/mewacho/doctype/mewacho_payment/mewacho_payment.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2025, hailemaryammecca@gmail.com and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class MewachoPayment(Document):
-	pass
+	def after_insert(self):
+		if not self.amount or not self.member:
+			frappe.throw("Amount and Member are required.")
+
+		amount = self.amount
+		member_doc = frappe.get_doc('Mewacho Member', self.member)
+		member_doc.total_payment_received += amount
+
+		for payment_term in member_doc.payment_terms:
+			if not payment_term.is_paid and amount >= payment_term.amount:
+				payment_term.db_set('is_paid', True)
+				amount -= payment_term.amount
+		for penality in member_doc.penality_list:
+			if penality.status == 'Unpaid' and amount >= penality.amount:
+				penality.db_set('status', 'Paid')
+				amount-= penality.amount
+	
+		member_doc.save()
+		frappe.msgprint("Payment Status updated Successfully!")
