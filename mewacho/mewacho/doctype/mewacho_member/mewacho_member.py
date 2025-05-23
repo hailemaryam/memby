43b68d9da6a2ea9c0baedddf9508bac40e0e07f7
hailemaryam <hailemaryammecca@gmail.com>
2025-05-23 21:07:22 +0300

    member payment calculator added for editing purpose

--- mewacho/mewacho/doctype/mewacho_member/mewacho_member.py
+++ mewacho/mewacho/doctype/mewacho_member/mewacho_member.py
@@ -4,8 +4,32 @@
 import frappe
 from frappe.model.document import Document
 
-
 class MewachoMember(Document):
-	def before_insert(self):
-		if not self.registration_date:
-			self.registration_date = frappe.utils.today()
+    def validate(self):
+        paid_total = 0
+        unpaid_total = 0
+
+        for payment in self.monthly_payments:
+            if payment.is_paid:
+                paid_total += payment.amount
+            else:
+                unpaid_total += payment.amount
+
+        for other_payment in self.other_payments:
+            if other_payment.status == "Paid":
+                paid_total += other_payment.amount
+            elif other_payment.status == "Unpaid":
+                unpaid_total += other_payment.amount
+
+        remaining_total = self.total_payment_received - paid_total
+
+        # Try to mark some unpaid payments as paid if funds allow
+        if remaining_total > 0:
+            for other_payment in self.other_payments:
+                if other_payment.status == "Unpaid" and remaining_total >= other_payment.amount:
+                    other_payment.status = "Paid"
+                    remaining_total -= other_payment.amount
+                    unpaid_total -= other_payment.amount
+
+        self.unpaid_total = unpaid_total
+        self.remaining_total = remaining_total
