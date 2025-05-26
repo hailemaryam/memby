fd1ff061b1d8ac6a48f79846b7c86bcf09594f29
hailemaryam <hailemaryammecca@gmail.com>
2025-05-26 16:53:29 +0300

    id with qr printing and qr url page added

--- mewacho/mewacho/doctype/mewacho_member/mewacho_member.py
+++ mewacho/mewacho/doctype/mewacho_member/mewacho_member.py
@@ -2,9 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class MewachoMember(Document):
+class MewachoMember(WebsiteGenerator):
     def validate(self):
         calculate_remaining_and_unpaid_total(self)
         mark_unpaid_payments(self)
