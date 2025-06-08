d746dbe719fb6d7588848a26151c9cec0190dc6c
hailemaryam <hailemaryammecca@gmail.com>
2025-06-08 11:37:21 +0300

    app logo added

--- memby/hooks.py
+++ memby/hooks.py
@@ -11,15 +11,15 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "memby",
-# 		"logo": "/assets/memby/logo.png",
-# 		"title": "Memby",
-# 		"route": "/memby",
-# 		"has_permission": "memby.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "memby",
+		"logo": "/assets/memby/logo.png",
+		"title": "Memby",
+		"route": "/memby",
+		"has_permission": "memby.api.permission.has_app_permission"
+	}
+]
 
 # Includes in <head>
 # ------------------
